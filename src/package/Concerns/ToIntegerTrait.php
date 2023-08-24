501f74de8628eaa4f855aaf36ff5590507411459
toarupi0318 <toarupg0318@gmail.com>
2023-08-24 19:38:26 +0900

    add: ToIntegerTrait

---
+++ src/package/Concerns/ToIntegerTrait.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Toarupg0318\TypeAlchemist\Concerns;
+
+/**
+ * @property mixed $value
+ */
+trait ToIntegerTrait
+{
+//    public function __toSafeInt(): int|null
+//    {
+//
+//    }
+//
+//    public function __toStrictInt(): int
+//    {
+//
+//    }
+}
