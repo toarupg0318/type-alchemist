94b27dce1b111a275b530b5f3b1ce1e0d5a650cd
toarupi0318 <toarupg0318@gmail.com>
2023-08-24 21:33:19 +0900

    fix: traits to no longer accept exception as an argument

--- tests/Unit/ToStringTraitTest.php
+++ tests/Unit/ToStringTraitTest.php
@@ -51,16 +51,59 @@ it('ToStringTrait performs correctly.', function (mixed $expect, string|null $re
 it(
     description: 'tests strict mode throws exception correctly if Throwable class-string passed.',
     closure: function (
-        string|null $passedExceptionClassName,
-        Throwable|null $actualException
+        mixed $input,
+        string|TypeError $output
     ) {
-        expect(
-         fn () => alchemy(-1)->__toStrictPositiveInt($passedExceptionClassName))
-            ->toThrow($actualException);
+        if ($output instanceof Throwable) {
+            expect(
+                fn () => alchemy($input)->__toStrictString())
+                ->toThrow($output);
+        } else {
+            if (is_resource($input)) {
+                expect(alchemy($input)->__toStrictString())
+                    ->toBeString($output);
+            } else {
+                expect(alchemy($input)->__toStrictString())
+                    ->toBe($output);
+            }
+        }
     }
 )
     ->with([
-        [LogicException::class, new LogicException()],
-        [RuntimeException::class, new RuntimeException()],
-        [null, new TypeError()],
+        [0, '0'],
+        [-1, '-1'],
+        [1.0, '1'],
+        [-3.14, '-3.14'],
+        ['true', 'true'],
+        ['false', 'false'],
+        [true, '1'],
+        //[false, ''],
+        //[false, null],
+        [[], new TypeError()],
+        [[1, 2, 3], new TypeError()],
+        [['key' => 'value'], new TypeError()],
+        [new stdClass(), new TypeError()],
+        [null, ''],
+        [
+            (function () {
+                $file = 'file.txt';
+                file_put_contents($file, 'Hello World!');
+                $resource = fopen($file, 'r');
+                if (! is_resource($resource)) {
+                    throw new Error();
+                }
+                return $resource;
+            })(),
+            ''
+        ],
+        [function () {return 'test';}, new TypeError()],
+        [
+            new class () implements Stringable {
+                public function __toString(): string
+                {
+                    return 'hoge';
+                }
+            },
+            'hoge'
+        ],
     ]);
